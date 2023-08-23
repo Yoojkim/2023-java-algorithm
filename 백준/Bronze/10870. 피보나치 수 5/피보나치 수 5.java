0db21fcfc95abe29a6e429d50c00384469d29018
YJ KIM <yjee0215@icloud.com>
2023-08-24 03:13:56 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -2,17 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
+    static boolean[] visit;
+    static int[] values;
     
     static int pibo(int n){
         if(n==0 || n==1)
             return n;
         
-        return pibo(n-1)+pibo(n-2);
+        if(visit[n])
+            return values[n];
+        
+        int val=pibo(n-2)+pibo(n-1);
+        visit[n]=true;
+        values[n]=val;
+        
+        return val;
     }
         
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
+        visit=new boolean[n+1];
+        values=new int[n+1];
         
         System.out.print(pibo(n));
     }
