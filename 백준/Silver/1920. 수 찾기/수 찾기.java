941202816923839416214ab441a6c4d6e6725a6f
YJ KIM <yjee0215@icloud.com>
2023-09-01 03:39:38 +0900

    [Silver IV] Title: 수 찾기, Time: 764 ms, Memory: 46036 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,45 +1,48 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    public static void main(String[] args) throws Exception{
+public class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        List<Integer> field=new ArrayList();
-        for(int i=0;i<n;i++){
-            int num=Integer.parseInt(st.nextToken());
-            
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> field = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
             field.add(num);
         }
-        
-        //배열 정렬
-        Collections.sort(field);
-        
-        int m=Integer.parseInt(br.readLine());
-        st=new StringTokenizer(br.readLine());
-        for(int i=0;i<m;i++){
-            int num=Integer.parseInt(st.nextToken());
-            int ans=0;
 
-            int low=0; int high=n-1;
-            while(low<=high){
-                int mid=(low+high)/2;
-                
-                int midNum=field.get(mid);
-                if(midNum==num){
-                    ans=1;
+        Collections.sort(field);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            int ans = 0;
+
+            int low = 0;
+            int high = n - 1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+
+                int midNum = field.get(mid);
+                if (midNum == num) {
+                    ans = 1;
                     break;
                 }
-                
-                if(midNum>num)
-                    high=mid-1;
+
+                if (midNum > num)
+                    high = mid - 1;
                 else
-                    low=mid+1;
+                    low = mid + 1;
             }
 
-            System.out.println(ans);
+            answer.append(ans).append('\n'); // Append to StringBuilder
         }
+
+        System.out.print(answer);
     }
 }
