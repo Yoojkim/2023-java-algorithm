af744dc977bdc5331df74ead14b0bcbce7f89ac1
YJ KIM <yjee0215@icloud.com>
2023-09-13 18:17:18 +0900

    [Silver V] Title: 돌 게임, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -4,23 +4,28 @@ class Main{
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int n= scanner.nextInt();
+        int[] d=new int[n+1];
 
-        //상근-홀, 창영-짝
-        int cnt=0;
-        while(true){
-            if (n==0)
-                break;
+        dp(n, d);
+        if(d[n]%2==1){
+            System.out.print("SK");
+        }else{
+            System.out.print("CY");
+        }
+    }
 
-            if(n>=3)
-                n-=3;
-            else
-                n-=1;
-            cnt++;
+    static private int dp(int num, int[] d){
+        if (num==0)
+            return 0;
+        if (d[num]!=0)
+            return d[num];
+
+        if (num>=3){
+            d[num]=dp(num-3, d)+1;
+        } else{
+            d[num]=dp(num-1, d)+1;
         }
 
-        if(cnt%2==1)
-            System.out.print("SK");
-        else
-            System.out.println("CY");
+        return d[num];
     }
 }
