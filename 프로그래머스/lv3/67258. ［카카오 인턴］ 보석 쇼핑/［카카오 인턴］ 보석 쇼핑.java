bae764722420632eb6217e2a5f12cad2b2bd5e2a
YJ KIM <yjee0215@icloud.com>
2023-09-24 02:09:10 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 87.92 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,41 +1,51 @@
 import java.util.*;
 
 class Solution {
-
+    
     public int[] solution(String[] gems) {
-        Set<String> uniqueGems = new HashSet<>(Arrays.asList(gems));
-        int totalUniqueGems = uniqueGems.size();
-
-        Map<String, Integer> gemCount = new HashMap<>();
-        int left = 0, right = 0;
-        int[] ansRange = { 1, gems.length };
-        int minLength = Integer.MAX_VALUE;
-
-        while (right < gems.length) {
-            String currentGem = gems[right];
-
-            gemCount.put(currentGem, gemCount.getOrDefault(currentGem, 0) + 1);
-
-            while (gemCount.size() == totalUniqueGems) {
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
-                    ansRange[0] = left + 1;
-                    ansRange[1] = right + 1;
-                }
-
-                String leftGem = gems[left];
-                gemCount.put(leftGem, gemCount.get(leftGem) - 1);
-
-                if (gemCount.get(leftGem) == 0) {
-                    gemCount.remove(leftGem);
-                }
-
-                left++;
+        
+        //gems에서 중복 없는 모든 보석의 갯수
+        Set<String> nonDuplicatedGems=new HashSet<>(Arrays.asList(gems));
+        int cnt=nonDuplicatedGems.size();
+        
+        int ans=Integer.MAX_VALUE; int[] ansRange=null;
+        int left=0, right=0;
+        
+        //HashMap 생성
+        HashMap<String, Integer> cart=new HashMap();
+        
+        while(right<gems.length){
+            
+            String nowGem=gems[right];
+            
+            if(cart.containsKey(nowGem)){
+                cart.put(nowGem, cart.get(nowGem)+1);
+            } else{
+                cart.put(nowGem, 1);
             }
-
+            
+            //만족하는지 
+            while(cart.size()==cnt){
+                
+                if(ans>right-left+1){
+                    ans=right-left+1;
+                    
+                    int[] newAnsRange={left+1, right+1};
+                    ansRange=newAnsRange;
+                }
+                
+                //카트에서 현재 left 값을 지워줌 
+                cart.put(gems[left], cart.get(gems[left])-1);
+                
+                if(cart.get(gems[left])==0)
+                    cart.remove(gems[left]);
+                
+                left++;
+            } 
+                
             right++;
         }
-
+        
         return ansRange;
     }
 }
