d2b949c4656f97ff7902a239d7dfa5d6b0b7da4e
YJ KIM <yjee0215@icloud.com>
2023-10-09 23:19:27 +0900

    [level 4] Title: 징검다리, Time: 21.29 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        
+        Arrays.sort(rocks);
+        int ansSection=rocks.length-n+1;
+        
+        //Parametric search
+        int left=1; int right=distance;
+        int ans=distance;
+        while(left<=right){
+            int mid=(left+right)/2;
+            
+            if((distance/mid)<ansSection){
+                right=mid-1;
+                continue;
+            }
+            
+            if(isPossible(distance, mid, rocks, ansSection)){
+                left=mid+1;
+                ans=mid;
+            } else{
+                right=mid-1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    //이 부분 수정 필요 
+    private boolean isPossible(int distance, int section, int[] rocks, int ansSection){
+        int min=0;
+        int sectionCnt=0;
+        
+        for(int rock:rocks){
+            int dist=rock-min;
+            
+            if((dist/section)<1)
+                continue;
+            
+            sectionCnt++;
+            min=rock;
+        }
+        
+        int lastDist=distance-min;
+        
+        if((lastDist/section)>=1)
+            sectionCnt++;
+        
+        if(sectionCnt>=ansSection)
+            return true;
+        
+        return false;
+    }
+    
+}
