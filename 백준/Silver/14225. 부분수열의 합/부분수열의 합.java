5c279d0f3299eca9829c0927cf133da3c88cd55d
YJ KIM <yjee0215@icloud.com>
2023-11-04 22:21:22 +0900

    [Silver I] Title: 부분수열의 합, Time: 140 ms, Memory: 16196 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -12,36 +12,33 @@ public class Main{
             s[i]=Integer.parseInt(st.nextToken());
         }
         
-        List<Integer> result=new ArrayList<>();
-        backTracking(s, result, 0, 0, size);
-        System.out.println(getMinNumber(result));
+        boolean[] visited=new boolean[100000*20+1];
+        
+        backTracking(s, visited, 0, 0, size);
+        System.out.println(getMinNumber(visited));
     }
     
-    private static void backTracking(int[] s, List<Integer> result, int index, int sum, int size){
+    private static void backTracking(int[] s, boolean[] visited, int index, int sum, int size){
         if(index==size){
             return;
         }
         
-        result.add(sum+s[index]);
+        visited[sum+s[index]]=true;
         
-        backTracking(s, result, index+1, sum, size);
-        backTracking(s, result, index+1, sum+s[index], size);
+        backTracking(s, visited, index+1, sum, size);
+        backTracking(s, visited, index+1, sum+s[index], size);
     }
     
-    private static int getMinNumber(List<Integer> result){
-        Collections.sort(result);
+    private static int getMinNumber(boolean[] visited){
+        int res=-1;
         
-        int start=1;
-        for(int sum:result){
-            if(sum>start){
-                break;
-            }
+        for(int i=1;i<=100000*20;i++){
+           if(!visited[i]){
+               res=i;
+               break;
+           }
+        }            
         
-            if(sum==start){
-                start++;
-            }
-        }
-        
-        return start;
+        return res;
     }
 }
