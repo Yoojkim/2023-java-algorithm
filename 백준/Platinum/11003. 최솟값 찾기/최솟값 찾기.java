01ee941f532ac554cf017d82334f62ee52da54b9
YJ KIM <yjee0215@icloud.com>
2023-12-16 13:49:39 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2524 ms, Memory: 871460 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class Node implements Comparable<Node>{
+class Node {
     int index;
     int value;
 
@@ -9,15 +9,12 @@ class Node implements Comparable<Node>{
         this.index = index;
         this.value = value;
     }
-
-    public int compareTo(Node n){
-        return this.value-n.value;
-    }
 }
+
 class Main{
     static int N;
     static int L;
-    static PriorityQueue<Node> queue;
+    static Deque<Node> queue = new ArrayDeque<>();
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,46 +23,38 @@ class Main{
         N = Integer.parseInt(st.nextToken());
         L = Integer.parseInt(st.nextToken());
 
-        queue = new PriorityQueue<>();
         st = new StringTokenizer(br.readLine());
-        int res[] = new int[N+1];
+        int D[] = new int[N+1];
         for(int i=1;i<=N;i++){
-            queue.add(new Node(i, Integer.parseInt(st.nextToken())));
-
-            int start = getStartIndex(i); int end = i;
-            int value = getMinValue(start, end);
-            res[i] = value;
+            Node newNode = new Node(i, Integer.parseInt(st.nextToken()));
+            insert(newNode);
+            D[i] = queue.peekFirst().value;
         }
 
         StringJoiner sj = new StringJoiner(" ");
         for(int i=1;i<=N;i++){
-            sj.add(Integer.toString(res[i]));
+            sj.add(Integer.toString(D[i]));
         }
 
         System.out.println(sj);
     }
 
-    static private int getMinValue(int start, int end){
-        while(!queue.isEmpty()){
-            Node now = queue.peek();
+    private static void insert(Node A){
+        if(!queue.isEmpty() && queue.peekFirst().index == A.index-L+1-1){
+            queue.pollFirst();
+        }
+//        if(queue.size() == L){
+//            queue.pollFirst();
+//        }
 
-            if(now.index >= start && now.index<=end){
-                return now.value;
+        while(!queue.isEmpty()){
+            if(queue.peekLast().value < A.value){
+                break;
             }
 
-            queue.poll();
+            queue.pollLast();
         }
 
-        return -1; //실패 경우
-    }
-
-    static private int getStartIndex(int i){
-        int res = i-L+1;
-
-        if(res<=0){
-            res = 1;
-        }
-
-        return res;
+        queue.addLast(A);
     }
 }
