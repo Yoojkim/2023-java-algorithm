08b125eee5dc72cb503eb874a3f7e5e0ccd6015f
YJ KIM <yjee0215@icloud.com>
2024-01-03 23:05:45 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 512 ms, Memory: 91928 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static StringJoiner sj = new StringJoiner("\n");
+    static int sum = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        hanoi(1, 2, 3, N);
+        System.out.println(sum);
+        System.out.println(sj);
+    }
+
+    private static void hanoi(int start, int mid, int end, int section) {
+        //종료조건
+        if (section == 1) {
+            sj.add(start + " " + end);
+            sum++;
+
+            return;
+        }
+
+        hanoi(start, end, mid, section - 1);
+
+        sj.add(start + " " + end);
+        sum++;
+
+        hanoi(mid, start, end, section - 1);
+    }
+}
