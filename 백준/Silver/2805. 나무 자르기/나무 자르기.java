bb533479153e1962d825079c335fdc75fabfcc1f
YJ KIM <yjee0215@icloud.com>
2024-03-05 12:59:20 +0900

    [Silver II] Title: 나무 자르기, Time: 1048 ms, Memory: 120436 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -4,51 +4,47 @@ import java.io.*;
 class Main{
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] nums=br.readLine().split(" ");
-        int n=Integer.parseInt(nums[0]); long m=Long.parseLong(nums[1]);
-        
-        long[] ns=new long[n];
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++){
-            ns[i]=Long.parseLong(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] ns = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            ns[i] = Integer.parseInt(st.nextToken());
         }
-        
         Arrays.sort(ns);
-        long left=1; long right=ns[n-1];
-        long ans=0;
-        
+
+        int left = 0; int right = ns[ns.length-1];
+        int ans = 0;
         while(left<=right){
-            long mid=(left+right)/2;
-            
-            if(isPossible(ns, mid, m)){
-                if(ans<mid)
-                    ans=mid;
-                left=mid+1;
-            } else{
-                right=mid-1;
+            int mid = (left+right)/2;
+            long midM = 0;
+            for(int i=N-1;i>=0;i--){
+                if(mid>=ns[i]){
+                    break;
+                }
+
+                midM+=ns[i]-mid;
+            }
+
+            if(midM == M){
+                ans = mid;
+                break;
+            }
+
+            if(midM > M){
+                ans = mid;
+                left = mid+1;
+                continue;
+            }
+
+            if(midM < M){
+                right = mid-1;
+                continue;
             }
         }
-        
+
         System.out.print(ans);
-    }
-    
-    private static boolean isPossible(long[] ns, long size, long m){
-        
-        for(int i=ns.length-1; i>=0; i--){
-            long cnt=ns[i]-size;
-            
-            if(m<cnt)
-                m=0;
-            else
-                m-=cnt;
-            
-            if(m==0)
-                break;
-        }
-        
-        if(m==0)
-            return true;
-        else
-            return false;
     }
 }
