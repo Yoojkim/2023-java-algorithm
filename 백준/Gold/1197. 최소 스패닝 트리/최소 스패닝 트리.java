d28b37742c490db0ff632d52197722ff216efd22
YJ KIM <yjee0215@icloud.com>
2024-04-23 14:20:44 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 660 ms, Memory: 57020 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,100 +2,91 @@ import java.util.*;
 import java.io.*;
 
 class Edge implements Comparable<Edge>{
-    int node1;
-    int node2;
+    int a;
+    int b;
     int cost;
 
-    public Edge(int node1, int node2, int cost){
-        this.node1=node1;
-        this.node2=node2;
+    public Edge(int a, int b, int cost){
+        this.a=a;
+        this.b=b;
         this.cost=cost;
+
     }
 
-    //오름차순
-    public int compareTo(Edge e){
-        if(this.cost<e.cost)
-            return -1;
-        else if(this.cost==e.cost)
-            return 0;
-        else
-            return 1;
+    //오름차순 정렬
+    public int compareTo(Edge edge){
+        return this.cost-edge.cost;
     }
 }
-
-//Kruskal
-class Main{
-    static int n, m;
-
-    //간선 리스트
-    static List<Edge> edges=new ArrayList();
-    //부모 배열
+public class Main{
+    static int N, M;
     static int[] parents;
-
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] nums=br.readLine().split(" ");
-        n=Integer.parseInt(nums[0]); m=Integer.parseInt(nums[1]);
 
-        parents=new int[n+1];
-        //부모 배열 초기화(자기 자신으로)
-        for(int i=0;i<=n;i++){
-            parents[i]=i;
+        String[] inputs = br.readLine().split(" ");
+        N = Integer.parseInt(inputs[0]);
+        M = Integer.parseInt(inputs[1]);
+        parents = new int[N+1];
+
+        for(int i=1;i<=N;i++){
+            parents[i] = i;
         }
 
-        //간선 생성
-        for(int i=0;i<m;i++){
-            nums=br.readLine().split(" ");
-            int node1=Integer.parseInt(nums[0]); int node2=Integer.parseInt(nums[1]);
-            int cost=Integer.parseInt(nums[2]);
+        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        for(int i=0;i<M;i++){
+            String[] edge = br.readLine().split(" ");
+            int a = Integer.parseInt(edge[0]);
+            int b = Integer.parseInt(edge[1]);
+            int cost = Integer.parseInt(edge[2]);
 
-            Edge edge=new Edge(node1, node2, cost);
-            edges.add(edge);
+            queue.add(new Edge(a, b, cost));
         }
 
-        //정렬
-        Collections.sort(edges);
+        long res = getMst(queue);
+        System.out.println(res);
+    }
 
-        int cnt=0;
-        int sum=0;
-        //Edge cost 작은 순으로 Kruskal MST 수행
-        for(Edge edge:edges){
+    private static long getMst(PriorityQueue<Edge> queue){
+        long sum = 0;
 
-            if(cnt==n-1)
-                break;
+        while(!queue.isEmpty()){
+            Edge edge = queue.poll();
 
-            int node1=edge.node1; int node2= edge.node2;
-
-            if(find(node1)==find(node2))
+            if(isCycle(edge)){
                 continue;
+            }
 
-            //union
-            union(node2, node1);
             sum+=edge.cost;
-            cnt++; //간선 추가(간선은 n-1개)
         }
 
-        System.out.print(sum);
+        return sum;
     }
 
-    private static int find(int node){
-        if(parents[node]==node)
-            return node;
+    private static boolean isCycle(Edge edge){
+        int aParent = getParent(edge.a);
+        int bParent = getParent(edge.b);
 
-        return find(parents[node]);
-    }
+        if(aParent==bParent){
+            return true;
+        }
 
-    private static void union(int node1, int node2){
-        //node2 하위의 모든 node들을 node1으로 이동
-        int p1=find(node1);
-        int p2=find(node2);
+        //union
+        if(aParent<bParent){
+            parents[aParent] = bParent;
+        } else{
+            parents[bParent] = aParent;
+        }
         
-        //root가 더 작은 값으로 이동
-        if(p1>p2)
-            parents[p2]=p1;
-        else
-            parents[p1]=p2;
+        return false;
+    }
 
+    private static int getParent(int node){
+        if(node==parents[node]){
+            return node;
+        }
+
+        return getParent(parents[node]);
     }
 }
