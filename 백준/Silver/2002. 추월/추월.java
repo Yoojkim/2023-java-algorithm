d9ed6bdcc156d3f79b5463f8f5e142148354dcf3
YJ KIM <yjee0215@icloud.com>
2024-09-09 13:52:22 +0900

    [Silver II] Title: 추월, Time: 124 ms, Memory: 14680 KB -BaekjoonHub

---
+++ 백준/Silver/2002. 추월/추월.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        //in
+        Queue<String> in = new LinkedList<>();
+
+        //out
+        Queue<String> out = new LinkedList<>();
+        List<String> alreadyOut = new ArrayList<>();
+
+        for(int i=0;i<N;i++){
+            in.add(br.readLine());
+        }
+        for(int i=0;i<N;i++){
+            out.add(br.readLine());
+        }
+
+        int cnt = 0;
+        while(!out.isEmpty()){
+            String outCar = out.poll();
+            String inCar = getInCar(in, alreadyOut);
+
+            if(outCar.equals(inCar)){
+                in.poll();
+            } else {
+                cnt++;
+            }
+
+            alreadyOut.add(outCar);
+        }
+
+        System.out.print(cnt);
+    }
+
+    static String getInCar(Queue<String> in, List<String> alreadyOut){
+        while(!in.isEmpty()){
+            String inCar = in.peek();
+
+            if(alreadyOut.contains(inCar)){
+                in.poll();
+                continue;
+            } else {
+                return inCar;
+            }
+        }
+
+        return "";
+    }
+}
