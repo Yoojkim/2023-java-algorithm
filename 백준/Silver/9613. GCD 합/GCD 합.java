67076b00002fcb21108111eecfcb0e0e73cf8dc2
YJ KIM <yjee0215@icloud.com>
2024-11-04 12:17:25 +0900

    [Silver IV] Title: GCD 합, Time: 352 ms, Memory: 29524 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.java
+++ 백준/Silver/9613. GCD 합/GCD 합.java
@@ -1,33 +1,36 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    public static void main(String[] args) throws Exception{
+public class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
         StringJoiner sj = new StringJoiner("\n");
-        while(T-->0){
-            st = new StringTokenizer(br.readLine());
 
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int[] numbers = new int[N];
-
-            for(int i=0;i<N;i++){
+            
+            for (int i = 0; i < N; i++) {
                 numbers[i] = Integer.parseInt(st.nextToken());
             }
 
-            long sum =0;
-            for(int i=0;i<N;i++){
-                int a= numbers[i];
-                List<Integer> divisor = getDivisors(a);
-                for(int j=1;i+j<N;j++){
-                    int b = numbers[i+j];
+            long sum = 0;
+            Map<Integer, List<Integer>> divisorCache = new HashMap<>();
 
-                    for(int k=divisor.size()-1;k>=0;k--){
-                        int aDivisor = divisor.get(k);
-                        if(b%aDivisor == 0){
+            for (int i = 0; i < N; i++) {
+                int a = numbers[i];
+                List<Integer> divisors = divisorCache.computeIfAbsent(a, Main::getDivisors);
+                
+                for (int j = 1; i + j < N; j++) {
+                    int b = numbers[i + j];
+                    
+                    for (int k = divisors.size() - 1; k >= 0; k--) {
+                        int aDivisor = divisors.get(k);
+                        if (b % aDivisor == 0) {
                             sum += aDivisor;
                             break;
                         }
@@ -41,17 +44,19 @@ public class Main{
         System.out.print(sj);
     }
 
-    static List<Integer> getDivisors(int num){
+    static List<Integer> getDivisors(int num) {
         List<Integer> ans = new ArrayList<>();
 
-        for(int i=1;i<=num/2;i++){
-            if(num%i == 0){
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
                 ans.add(i);
+                if (i != num / i) {
+                    ans.add(num / i);
+                }
             }
         }
 
-        ans.add(num);
-
+        Collections.sort(ans); // 내림차순으로 사용하려면 정렬
         return ans;
     }
 }
