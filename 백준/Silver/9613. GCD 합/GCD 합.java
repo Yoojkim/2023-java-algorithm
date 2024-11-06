20cc41b0e12bfe12c054330ebbcdc78eb1626e01
YJ KIM <yjee0215@icloud.com>
2024-11-06 12:16:12 +0900

    [Silver IV] Title: GCD 합, Time: 240 ms, Memory: 23260 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.java
+++ 백준/Silver/9613. GCD 합/GCD 합.java
@@ -1,40 +1,32 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+public class Main{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
         StringJoiner sj = new StringJoiner("\n");
-
-        while (T-- > 0) {
+        while(T-->0){
             st = new StringTokenizer(br.readLine());
+
             int N = Integer.parseInt(st.nextToken());
             int[] numbers = new int[N];
-            
-            for (int i = 0; i < N; i++) {
-                numbers[i] = Integer.parseInt(st.nextToken());
+
+            for(int i=0;i<N;i++){
+                numbers[i] = Integer.parseInt(st.nextToken());;
             }
 
-            long sum = 0;
-            Map<Integer, List<Integer>> divisorCache = new HashMap<>();
+            long sum =0;
+            for(int i=0;i<N-1;i++){
+                int a= numbers[i];
 
-            for (int i = 0; i < N; i++) {
-                int a = numbers[i];
-                List<Integer> divisors = divisorCache.computeIfAbsent(a, Main::getDivisors);
-                
-                for (int j = 1; i + j < N; j++) {
-                    int b = numbers[i + j];
-                    
-                    for (int k = divisors.size() - 1; k >= 0; k--) {
-                        int aDivisor = divisors.get(k);
-                        if (b % aDivisor == 0) {
-                            sum += aDivisor;
-                            break;
-                        }
-                    }
+                for(int j=i+1;j<N;j++){
+                    int b = numbers[j];
+
+                    int gcd = getGCD(Math.max(a, b), Math.min(a,b));
+                    sum+= gcd;
                 }
             }
 
@@ -44,19 +36,14 @@ public class Main {
         System.out.print(sj);
     }
 
-    static List<Integer> getDivisors(int num) {
-        List<Integer> ans = new ArrayList<>();
+    static int getGCD(int max, int min){
+        while(min!=0){
+            int remainder = max%min;
 
-        for (int i = 1; i * i <= num; i++) {
-            if (num % i == 0) {
-                ans.add(i);
-                if (i != num / i) {
-                    ans.add(num / i);
-                }
-            }
+            max = min;
+            min = remainder;
         }
 
-        Collections.sort(ans); // 내림차순으로 사용하려면 정렬
-        return ans;
+        return max;
     }
 }
